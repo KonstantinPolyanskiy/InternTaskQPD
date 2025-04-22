fd406a48c63e2b47f28ac4de01bf9a8cee08b91b
k.polyanskiy <k.polyanskiy@qpdev.ru>
2025-04-22 11:59:35 +0300

    Revert "add car service and docker"
    This reverts commit 36e7fb398137ac8cae7d6af400cf8f44ed6ef08e.

--- CarService/Program.cs
+++ CarService/Program.cs
@@ -1,45 +1,41 @@
-using System.Reflection;
-using CarService.Daos.CarDao;
-using CarService.Daos.CarDao.EntityCarDao;
-using CarService.Models.Car.Mapping;
-using CarService.Services.CarService;
-using Microsoft.EntityFrameworkCore;
-using Serilog;
-
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Host.UseSerilog((ctx, lc) => lc
-    .ReadFrom.Configuration(ctx.Configuration));
-
-builder.Services.AddDbContext<CarDbContext>(options =>
-{
-    options.UseNpgsql(
-        builder.Configuration.GetConnectionString("Default"));
-});
-
-builder.Services.AddScoped<ICarDao, EntityCarDao>();
-builder.Services.AddScoped<ICarService, CarService.Services.CarService.CarService>();   
-
-builder.Services.AddAutoMapper(typeof(CarMappingProfile));
-
-builder.Services.AddControllers();
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("DefaultPolicy", policy =>
-    {
-        policy.AllowAnyOrigin()
-            .AllowAnyMethod()
-            .AllowAnyHeader();
-    });
-});
-
-builder.Services.AddEndpointsApiExplorer();
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-app.UseCors("DefaultPolicy");
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
 
-app.MapControllers();
+app.UseHttpsRedirection();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+    {
+        var forecast = Enumerable.Range(1, 5).Select(index =>
+                new WeatherForecast
+                (
+                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    Random.Shared.Next(-20, 55),
+                    summaries[Random.Shared.Next(summaries.Length)]
+                ))
+            .ToArray();
+        return forecast;
+    })
+    .WithName("GetWeatherForecast");
 
 app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
