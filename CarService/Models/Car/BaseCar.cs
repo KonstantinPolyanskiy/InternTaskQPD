36e7fb398137ac8cae7d6af400cf8f44ed6ef08e
k.polyanskiy <k.polyanskiy@qpdev.ru>
2025-04-22 11:46:41 +0300

    add car service and docker

---
+++ CarService/Models/Car/BaseCar.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+using CarService.Models.Car.Requests;
+
+namespace CarService.Models.Car;
+
+public class BaseCar : ICar
+{
+    public BaseCar() { }
+    
+    public int? Id { get; set; }
+    public string? Brand { get; set; }
+    public string? Color { get; set; }
+    public decimal Price { get; set; }
+    public string? Photo { get; set; }
+}
---
+++ CarService/Services/CarService/ICarService.cs
@@ -0,0 +1,14 @@
+﻿using CarService.Api.RestControllers.CarController;
+using CarService.Models.Car;
+using CarService.Models.Car.Requests;
+
+namespace CarService.Services.CarService;
+
+public interface ICarService
+{
+    Task<ICar> CreateCarAsync(CreateCarRequestDto requestDto);
+    Task<bool?> DeleteCarAsync(int id);
+    Task<ICar> UpdateCarAsync(int id, PatchUpdateCarRequestDto newCar);
+    Task<ICar> GetCarByIdAsync(int id);
+    Task<IList<ICar>> GetAllCarsAsync();
+}
