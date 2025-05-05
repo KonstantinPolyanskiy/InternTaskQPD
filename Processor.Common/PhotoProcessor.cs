c83295393b9184e686a84357dd2cfbd1290b47ff
Konstantin Polyanskiy <polyanskiy.main@list.ru>
2025-05-05 11:08:59 +0300

    Delete artefacts

--- Processor.Common/PhotoProcessor.cs
+++
@@ -1,9 +0,0 @@
-﻿using Backend.App.Models.Business;
-using Backend.App.Models.PhotoModels;
-using Enum.Common;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Routing;
-
-namespace Processor.Common;
-
-
