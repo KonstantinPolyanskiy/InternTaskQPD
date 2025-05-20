5125995ba8286a625a8ae876e6147d0c8854b791
Konstantin Polyanskiy <polyanskiy.main@list.ru>
2025-05-20 16:27:14 +0300

    Delete Backend.Api directory
    delete artefacts

--- Backend.Api/Models/Requests/PhotoRequests.cs
+++
@@ -1,7 +0,0 @@
-namespace Backend.Api.Models.Requests;
-
-/// <summary> Запрос на установку фото для машины </summary>
-public record SetPhotoToCarRequest
-{
-    public required IFormFile Photo { get; init; }
-}
