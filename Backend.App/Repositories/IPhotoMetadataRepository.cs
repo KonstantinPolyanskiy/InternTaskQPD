efb33378b2f89d9d50e7622c04a7badd84c5f8fa
Konstantin Polyanskiy <polyanskiy.main@list.ru>
2025-05-20 16:27:33 +0300

    Delete Backend.App directory
    delete artefacts

--- Backend.App/Repositories/IPhotoMetadataRepository.cs
+++
@@ -1,9 +0,0 @@
-using Backend.App.Models.Dto;
-
-namespace Backend.App.Repositories;
-
-public interface IPhotoMetadataRepository
-{
-    public Task<PhotoMetadataDto> SavePhotoMetadataAsync(PhotoMetadataDto dto);
-    public Task<PhotoMetadataDto?> GetPhotoMetadataAsync(int metadataId);
-}
